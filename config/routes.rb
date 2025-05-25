8fd5be909ea1c77c8eca22ad027845390905a20b
matthew-io <xyzmonte@protonmail.com>
2025-05-25 23:21:46 +0100

    added inventory count and subscriber notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
- resources :products
+ resources :products do
+   resources :subscribers, only: [ :create ]
+   resource :unsubscribe, only: [ :show ]
+ end
  root "products#index"
 end
