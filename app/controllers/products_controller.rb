7623d20107d8c46650265c5b13a38b3c18dfbc7c
matthew-io <xyzmonte@protonmail.com>
2025-05-25 12:43:39 +0100

    added authentication, file upload and locale internationalization

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit update ]
+  before_action :set_product, only: %i[ show edit update destroy ]
+  allow_unauthenticated_access only: %i[ index show ]
 
   def index
     @products = Product.all
@@ -32,12 +33,17 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
   private
     def set_product
       @product = Product.find(params[:id])
     end
 
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :description, :featured_image ])
     end
 end
